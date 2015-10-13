2bb6790fddd89325baf0c1deee7b8f0b64ec77db
B50calabrese <antoniobrese@aol.com>
2015-10-13 11:03:19 -0400

    Initial commit of the Android frontend.

---
+++ android/app/src/main/java/edu/gatech/gtevents/gtevents/eventcreationscreen/EventCreationScreenActivity.java
@@ -0,0 +1,41 @@
+package edu.gatech.gtevents.gtevents.eventcreationscreen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.gatech.gtevents.gtevents.R;
+
+/**
+ * Created by Antonio Calabrese on 10/13/2015.
+ */
+public class EventCreationScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ android/app/src/main/java/edu/gatech/gtevents/gtevents/loginscreen/CredentialVerifier.java
@@ -0,0 +1,16 @@
+package edu.gatech.gtevents.gtevents.loginscreen;
+
+/**
+ * Verifies that the credentials are correct.
+ */
+public class CredentialVerifier {
+
+    /**
+     * Verifies that a pair of GT credentials is correct and valid.
+     * @return true if the credentials were valid.
+     */
+    public static boolean verify(String username, String password) {
+        // TODO (acalabrese): I need to implement this feature and see hwo GT API handles it.
+        return true;
+    }
+}
